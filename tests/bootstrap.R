94ccde33f42bbd408b32344db186d3782c675e87
David Bronaugh <bronaugh@uvic.ca>
2014-06-19 11:25:14 -0700

    Fix tests on Windows; give proper credit to authors whose code was modified to make PCICt; update some tests.

--- tests/bootstrap.R
+++ tests/bootstrap.R
@@ -1,6 +1,9 @@
-library(RUnit)
 
-## Run all the tests
-testsuite <- defineTestSuite("PCICt", dirs="./", testFileRegexp = "^test_functions.R$", testFuncRegexp = "^PCICt.test.+")
-PCICt.test.result <- runTestSuite(testsuite, useOwnErrorHandler=F)
-printTextProtocol(PCICt.test.result)
+if(require("RUnit", quietly=TRUE)) {
+  ## Run all the tests
+  wd <- getwd()
+  testsuite <- defineTestSuite("PCICt", dirs=wd, testFileRegexp = "^test_functions.R$", testFuncRegexp = "^PCICt.test.+")
+  PCICt.test.result <- runTestSuite(testsuite, useOwnErrorHandler=F)
+  printTextProtocol(PCICt.test.result)
+  stopifnot(climdex.pcic.test.result$PCICt$nFail == 0 && climdex.pcic.test.result$PCICt$nErr == 0)
+}
